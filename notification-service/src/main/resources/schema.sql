62459a2f8b2224d780ecabb4c8579fa98d2d65e4
Viktor Novyk <novikvictor@gmail.com>
2026-03-05 20:43:28 +0200

    Clean up schema

--- notification-service/src/main/resources/schema.sql
+++ notification-service/src/main/resources/schema.sql
@@ -7,16 +7,7 @@ CREATE TABLE IF NOT EXISTS notifications (
     is_read BOOLEAN NOT NULL DEFAULT FALSE
 );
 
-ALTER TABLE IF EXISTS notifications DROP COLUMN IF EXISTS latitude;
-ALTER TABLE IF EXISTS notifications DROP COLUMN IF EXISTS longitude;
-ALTER TABLE IF EXISTS notifications ADD COLUMN IF NOT EXISTS group_id BIGINT;
-ALTER TABLE IF EXISTS notifications DROP COLUMN IF EXISTS group_name;
-ALTER TABLE IF EXISTS notifications DROP COLUMN IF EXISTS group_link;
-ALTER TABLE IF EXISTS notifications ADD COLUMN IF NOT EXISTS is_read BOOLEAN NOT NULL DEFAULT FALSE;
-UPDATE notifications SET is_read = FALSE WHERE is_read IS NULL;
-
 CREATE INDEX IF NOT EXISTS idx_notifications_timestamp ON notifications(timestamp DESC);
-DROP INDEX IF EXISTS idx_notifications_group_link_timestamp;
 CREATE INDEX IF NOT EXISTS idx_notifications_group_id_timestamp ON notifications(group_id, timestamp DESC);
 CREATE INDEX IF NOT EXISTS idx_notifications_is_read_timestamp ON notifications(is_read, timestamp DESC);
 CREATE INDEX IF NOT EXISTS idx_notifications_group_id_is_read_timestamp ON notifications(group_id, is_read, timestamp DESC);
